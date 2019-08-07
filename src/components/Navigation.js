50ed709e5666edf098a2aac68c1bdfab054784ef
stylessh <stylesshjs@gmail.com>
2019-08-07 00:16:46 -0400

    the bootstrap theme has added

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,14 +4,14 @@ import { Link } from "react-router-dom";
 class Navigation extends Component{
   render(){
     return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container">
           <Link className="navbar-brand" to="/">Cocina Alacio</Link>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
+            <ul className="navbar-nav ml-auto">
               <li className="nav-item active">
                 <Link className="nav-link" to="/">Inicio</Link>
               </li>
